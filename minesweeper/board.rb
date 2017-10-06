e1e1018657a0196d2fe645cec79d933389e54331
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-10-05 17:17:46 -0700

    Set empty grid, initialize, and bracket method

--- minesweeper/board.rb
+++ minesweeper/board.rb
@@ -1,3 +1,28 @@
 def Board
+  attr_reader :grid
 
+  def self.empty_grid
+    @grid = Array.new(9) do
+      Array.new(9) { Tile.new(0) }
+    end
+    grid
+  end
+
+  def intialize(grid = self.empty_grid)
+    @grid = grid
+  end
+
+  def bombs
+
+  end
+
+  def [](pos)
+    x, y = pos
+    grid[x][y]
+  end
+
+  # def []=(pos, value)
+  #   x, y = pos
+  #   grid[x][y]
+  # end
 end
